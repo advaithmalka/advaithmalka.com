3d1fd0863257b0068abb8cdaa614f1b80df38fb0
AdvaithM26 <65194982+AdvaithM26@users.noreply.github.com>
2021-03-18 17:39:47 -0400

    add lit bot to projects + card updates

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,41 +5,41 @@ import "tippy.js/dist/tippy.css";
 import { Link } from "react-router-dom";
 
 const Card = props => {
-	const allBadges = props.badges.map(badge => {
-		return badgeIdentifiers[badge];
-	});
+  const allBadges = props.badges.map(badge => {
+    return badgeIdentifiers[badge];
+  });
 
-	return (
-		<div className="col-lg-4 col-md-6">
-			<div className="lit-card my-4" style={props.style}>
-				<img
-					className="lit-card-img"
-					src={`${process.env.PUBLIC_URL}/img/${props.img}`}
-					alt="Project"
-				/>
-				{allBadges}
-				<div className="lit-card-body">
-					<h1 className="lit-card-title mb-4">{props.title}</h1>
-					<p className="lit-card-text mb-4">{props.desc}</p>
-					<p className="fs-13">{props.date}</p>
-					{props.link.startsWith("http") ? (
-						<a
-							className="lit-card-btn"
-							target="_blank"
-							rel="noreferrer noopener"
-							href={props.link}
-						>
-							View
-						</a>
-					) : (
-						<Link to={props.link} className="text-decoration-none">
-							<button className="lit-card-btn router-link">View</button>
-						</Link>
-					)}
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="col-lg-4 col-md-6">
+      <div className="lit-card my-4" style={props.style}>
+        <img
+          className="lit-card-img"
+          src={`${process.env.PUBLIC_URL}/img/${props.img}`}
+          alt="Project"
+        />
+        {allBadges}
+        <div className="lit-card-body">
+          <h1 className="lit-card-title mb-3 f-inter ">{props.title}</h1>
+          <p className="lit-card-text mb-4">{props.desc}</p>
+          <p className="fs-13">{props.date}</p>
+          {props.link.startsWith("http") ? (
+            <a
+              className="lit-card-btn"
+              target="_blank"
+              rel="noreferrer noopener"
+              href={props.link}
+            >
+              View
+            </a>
+          ) : (
+            <Link to={props.link} className="text-decoration-none">
+              <button className="lit-card-btn router-link">View</button>
+            </Link>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- src/views/Blog.js
+++ src/views/Blog.js
@@ -3,65 +3,65 @@ import BlogPost from "../components/BlogPost";
 import BlogBackdrop from "../assets/img/blog.png";
 import { onScroll } from "../util/onScroll";
 const Blog = () => {
-	const blogPostsRef = useRef();
-	const blogRef = useRef();
-	const [blogPosts, setBlogPosts] = useState([]);
-	const [loading, setLoading] = useState(true);
-	useEffect(() => {
-		document.title = "Blog";
-		window.addEventListener("scroll", () => onScroll(blogRef));
-		(async function () {
-			let data = await fetch(
-				"https://medium-widget-api.pixelpoint.io/api/v1/articles?resource=https%3A%2F%2Fmedium.com%2F%40advaithmalka&limit=10"
-			);
-			data = await data.json();
-			setBlogPosts(data);
-			setLoading(false);
+  const blogPostsRef = useRef();
+  const blogRef = useRef();
+  const [blogPosts, setBlogPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    document.title = "Blog";
+    window.addEventListener("scroll", () => onScroll(blogRef));
+    (async function () {
+      let data = await fetch(
+        "https://medium-widget-api.pixelpoint.io/api/v1/articles?resource=https%3A%2F%2Fmedium.com%2F%40advaithmalka"
+      );
+      data = await data.json();
+      setBlogPosts(data);
+      setLoading(false);
 
-			return () =>
-				window.removeEventListener("scroll", () => onScroll(blogRef));
-		})();
-	}, []);
-	const styles = {
-		height: 370,
-		backgroundColor: "#071F3D",
-		backgroundImage: `url(${BlogBackdrop})`,
-		backgroundSize: "cover",
-		backgroundPosition: `0px ${Math.round(window.scrollY * -0.65) - 0}px`,
-	};
+      return () =>
+        window.removeEventListener("scroll", () => onScroll(blogRef));
+    })();
+  }, []);
+  const styles = {
+    height: 370,
+    backgroundColor: "#071F3D",
+    backgroundImage: `url(${BlogBackdrop})`,
+    backgroundSize: "cover",
+    backgroundPosition: `0px ${Math.round(window.scrollY * -0.65) - 0}px`,
+  };
 
-	return (
-		<>
-			<div
-				ref={blogRef}
-				className="container-fluid text-center text-white flex items-center"
-				style={styles}
-			>
-				<div className="mx-auto">
-					<h1 className="text-7xl my-6">Blog</h1>
-					<p className="lead">Advaith Malka's latest blog posts</p>
-				</div>
-			</div>
-			<div className="container px-6">
-				<div className="max-w-3xl mx-auto">
-					<div ref={blogPostsRef} className="my-7" id="blog-posts">
-						{loading ? (
-							<div
-								className="flex items-center justify-center"
-								style={{ height: "70vh" }}
-							>
-								<div className="lit-spinner-big"></div>
-							</div>
-						) : (
-							blogPosts.map((post, idx) => {
-								return <BlogPost key={idx} post={post} />;
-							})
-						)}
-					</div>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div
+        ref={blogRef}
+        className="container-fluid text-center text-white flex items-center"
+        style={styles}
+      >
+        <div className="mx-auto">
+          <h1 className="text-7xl my-6">Blog</h1>
+          <p className="lead">Advaith Malka's latest blog posts</p>
+        </div>
+      </div>
+      <div className="container px-6">
+        <div className="max-w-3xl mx-auto">
+          <div ref={blogPostsRef} className="my-7" id="blog-posts">
+            {loading ? (
+              <div
+                className="flex items-center justify-center"
+                style={{ height: "70vh" }}
+              >
+                <div className="lit-spinner-big"></div>
+              </div>
+            ) : (
+              blogPosts.map((post, idx) => {
+                return <BlogPost key={idx} post={post} />;
+              })
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Blog;
